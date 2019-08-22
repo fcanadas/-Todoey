9f07cb62f7cc76eff2699e85a02b77f2a3e6f852
IHDF_Fran <ihdf_fran@macbook-pro-de-ihdf-fran.local>
2019-08-22 13:30:50 +0200

    Convert to MVC design pattern

---
+++ Todoey/Data Model/Item.swift
@@ -0,0 +1,16 @@
+//
+//  Item.swift
+//  Todoey
+//
+//  Created by IHDF_Fran on 22/08/2019.
+//  Copyright © 2019 FranciscoCanadasQuesada. All rights reserved.
+//
+
+import Foundation
+
+class Item{
+    
+    var title : String = ""
+    var done : Bool = false
+    
+}
